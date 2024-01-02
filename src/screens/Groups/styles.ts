38c27d33f7049b00223b9ee1049dcd60aeb5104b
Sh4rk-dev <renanrapace13@gmail.com>
2024-01-01 23:18:53 -0300

    style config from app

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+
+export const Title = styled.Text`
+    color: #f00;
+    font-size: 32px;
+
+`;
