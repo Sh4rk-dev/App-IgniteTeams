e168810dbe17d832e723ec5eb7dae848b3ba007b
Renan <renanrapace13@gmail.com>
2024-01-08 19:17:30 -0300

    Creating a Dynamic Component and Installing the Icons

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components/native";
+import { CaretLeft } from "phosphor-react-native";
 
 export const  Container = styled.View`
     width: 100%;
 
+    margin-top: 16px;
     flex-direction: row;
     align-items: center;
     justify-content: center;
@@ -13,3 +15,11 @@ export const Logo = styled.Image `
     height: 55px;
 `;
 
+export const BackButton = styled.TouchableOpacity`
+    flex:1;    
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({theme}:any) => ({
+    color: theme.COLORS.WHITE,
+    size: 32,
+}))``;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@components/Header';
+import { Header } from '@components/header';
 import { Container } from './styles';
 
 export function Groups() {
