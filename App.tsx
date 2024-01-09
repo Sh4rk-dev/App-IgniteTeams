8021bcc6004234bcae1764c85d387ed73ef7b157
Renan <renanrapace13@gmail.com>
2024-01-09 02:21:01 -0300

    creating the "Classes" screen component.

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 import theme from "./src/theme";
 
 import { Groups } from "@screens/Groups";
-import { Loading } from "@components/loading";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
-import { Header } from '@components/header';
-import { Container } from './styles';
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+
+import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
-      <Header/>
+      <Header />
+      <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
