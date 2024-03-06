78080b3461c3ef92fe0583830bf7931775f205d6
Renan <renanrapace13@gmail.com>
2024-03-06 19:07:38 -0300

    creating dynamic components, styles and lists

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,33 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Button } from "@components/Button";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
+  console.log(groups);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+      <Button title="Criar nova Turma" />
     </Container>
   );
 }
