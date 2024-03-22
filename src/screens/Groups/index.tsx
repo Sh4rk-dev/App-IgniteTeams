7df72db4685637ed1c9028f4f95d6177c5208bfb
Renan <renanrapace13@gmail.com>
2024-03-22 19:41:00 -0300

    Creating route types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,16 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
-import { useState } from "react";
-import { FlatList } from "react-native";
-import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
-
-  console.log(groups);
 
   return (
     <Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,8 +1,9 @@
 import { Header } from "@components/Header";
-import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+
+import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
   return (
@@ -15,14 +16,9 @@ export function NewGroup() {
           title="Nova Turma"
           subTitle="crie a turma para adicionar as pessoas"
         />
-        <Input
-          placeholder="Nome da turma" 
-        />
+        <Input placeholder="Nome da turma" />
 
-        <Button 
-        title="Criar" 
-        style={{marginTop: 20}}
-        />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
